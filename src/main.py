2f63086867ef3a18aead555e81f057df81a939b9
jlgamez <jlgamezhernandez@gmail.com>
2022-05-04 20:06:11 +0200

    implemented logging

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-
+import logging
 import requests
 from bs4 import BeautifulSoup
 import injector_service.injector as injector
 import concurrent.futures
+
+# logging config
+logging.basicConfig(format='[%(asctime)s][%(levelname)s][%(threadName)s] %(message)s', level=logging.DEBUG)
+logger = logging.getLogger()
 
 base_url = 'https://www.webarcelona.net/barcelona-events/'
 web_barcelona_html = requests.get(base_url).text
@@ -11,7 +15,9 @@ soup = BeautifulSoup(web_barcelona_html, 'html.parser')
 
 def find_link_of_category(category_name):
     category_a_tag = soup.find('a', attrs={'title': category_name})
+    logger.debug('Extracting "' + category_name + '" link from ' + str(category_a_tag))
     category_link = category_a_tag.get('href')
+    logger.debug(category_name + ' link is ' + str(category_link))
     return category_link
 
 
@@ -33,12 +39,14 @@ def extract_event_title_and_url(event_li):
 
 def extract_event_description(event_li):
     event_p_tag = event_li.select_one('div.views-field-body p')
+    logger.debug('Got description from ' + str(event_p_tag))
     return str(event_p_tag.getText())
 
 
 def extract_event_date(event_li):
     event_time_tag = event_li.select_one('div.views-field-field-event-date time')
     date_time = str(event_time_tag.attrs.get('datetime'))
+    logger.debug('Extracted "' + str(date_time) + '" from ' + str(event_time_tag))
     idx = date_time.index('T')
     event_date = date_time[0:idx]
     return event_date
@@ -46,29 +54,38 @@ def extract_event_date(event_li):
 
 def extract_event_location(event_url):
     # to extract the location open the event link and extract from there
+    logger.debug('Accessing the event url to get the description')
     event_page_html = requests.get(event_url).text
     event_soup = BeautifulSoup(event_page_html, 'html.parser')
     div_address = event_soup.select_one('div.block-field-blocknodeeventfield-main-event-address div.field__item')
     # control for those cases without address
     try:
         event_address = str(div_address.string)
+        logger.debug('Extracted address "' + str(event_address) + '" from ' + str(div_address))
     except AttributeError:
         event_address = 'Barcelona'
+        logger.debug('Address div has no text. Setting location to ' + str(event_address))
+
     return event_address
 
 
 def scrape_data(event, category, exclude_events):
     request_body = {}
     title, url = extract_event_title_and_url(event)
+    logger.debug('Extracted title: "' + str(title) + '" and bookingLink: ' + str(url))
     # in case the title is already in the database, do not
     # proceed scraping more data
     if title in exclude_events:
+        logger.debug(category + ' title already in database. Interrupting scraping. "' + title + '"')
         return None
 
-    description = extract_event_description(event)
-    date = extract_event_date(event)
-    location = extract_event_location(url)
-    category_id = injector.get_category_id(category)
+    try:
+        description = extract_event_description(event)
+        date = extract_event_date(event)
+        location = extract_event_location(url)
+        category_id = injector.get_category_id(category)
+    except Exception as e:
+        logger.debug(e)
 
     request_body['title'] = title
     request_body['description'] = description
@@ -77,13 +94,15 @@ def scrape_data(event, category, exclude_events):
     request_body['bookingLink'] = url
     request_body['categoryId'] = category_id
 
+
+    logger.debug('Generated activity payload: ' + str(request_body))
     return request_body
 
 
 def add_activities_to_database(activity_category):
     # load titles that are already in the database to exclude them from the scraping session
     exclude_titles = injector.get_existing_titles_in_data_base(activity_category)
-
+    logger.debug('Extracted "' + str(activity_category) + '" titles from database: ' + str(exclude_titles))
     # map database category to webbarcelona.net category
     if activity_category == 'music':
         web_category = 'Concerts'
@@ -97,25 +116,24 @@ def add_activities_to_database(activity_category):
 
     # access the web category link and fetch all events inside
     events = get_events_list(web_category_link)
+    logger.debug('Extracted events from ' + str(web_category_link) + ': ' + str(events) )
 
     # scrape the required data from each event (if the title is already
     # in the database the returned object will be None
+    logger.debug('Starting data scraping from each event')
     for e in events:
         payload = scrape_data(e, activity_category, exclude_titles)
         # inject the data when the object is not None
-        print(str(payload))
         if payload is not None:
-            print('Injected\n')
             try:
                 injector.inject(payload)
+                logger.info('Payload successfully injected in database: ' + str(payload) )
             except Exception as e:
-                print(e)
-        else:
-            print('Not injected')
+                logger.debug(e)
 
 
 # scrape and inject the data asynchronously with a ThreadPoolExecutor
-categories_list = ['culture']
+categories_list = ['culture', 'music', 'city']
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
     executor.map(add_activities_to_database, categories_list)
