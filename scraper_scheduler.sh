e27f8d74619a087390f79898626441daac57af0f
jlgamez <jlgamezhernandez@gmail.com>
2022-05-06 22:49:13 +0200

    added scheduler script (to be executed by crontab)

---
+++ scraper_scheduler.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# run scraper with pipenv cmd
+. /Users/jl/.local/share/virtualenvs/webarcelona-scraper-RjhYpRh1/bin/activate
+python3 /Users/jl/dam_project_code/scraper/webarcelona-scraper/src/main.py
